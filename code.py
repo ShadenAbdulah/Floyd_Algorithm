55ff4667b15d3923c1b222c44dbe36ee9528f0e4
Shaden Abdullah <sh22m2015@gmail.com>
2023-07-05 09:07:08 +0300

    Update code.py

--- code.py
+++ code.py
@@ -16,28 +16,7 @@ def floyd(G):
         for i in range(len(G)):
             for j in range(len(G)):
                 distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
-                
-    print_solution(distance)
-
     global e; e = time.time()
-    
-def print_solution(G):
-    for i in range(len(G)):
-        for j in range(len(G)):
-            if(G[i][j] == INF):
-                print("INF", end=" ")
-            else:
-                print(G[i][j], end="  ")
-        print(" ")
-        
-def printGraph(G):
-    for i in range(len(G)):
-        v = i+1
-        for j in range(len(G[i])):
-            if G[i][j] != 0 and G[i][j] != 999: 
-                print("V%d ---%d---> V%d" % (v, G[i][j], j+1))
-            else: continue
-        print()
 
 def createGraph(size, case):
     G = [[random.randint(1, 20) for i in range(size)] for j in range(size)]
@@ -81,8 +60,6 @@ def drawCurve():
     plt.legend(loc="upper left")
     plt.show()
     
-
-
 def testWorst():
     j = 1
     print(" \n \nWorst Case \n")
@@ -108,7 +85,6 @@ def testAvg():
     print(" \n \nAverage Case \n")
     for i in vertexNum:
         G = createGraph(i, "Avg")
-        if i == 5: print(G)
         print("Senario %d: \n\tNumber of Vertex is %d" % (j, len(G))); floyd(G)
         print("\tRunning Time: %.5f microSec \n" % (1000000 * (e - s)))
         tAvg.append(1000000 * (e - s))
